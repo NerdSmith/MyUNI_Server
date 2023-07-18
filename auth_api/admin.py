9f7a8a860459fe77790b0593b56544020e8ad119
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-07-18 19:04:01 +0300

    added perm link

--- auth_api/admin.py
+++ auth_api/admin.py
@@ -8,6 +8,8 @@ User = get_user_model()
 # Register your models here.
 
 # https://realpython.com/manage-users-in-django-admin/
+# https://github.com/pgorecki/django-cancan
+
 
 admin.site.unregister(User)
 
@@ -17,5 +19,5 @@ class CustomUserAdmin(UserAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if not request.user.is_superuser and request.user.is_staff:
-            qs = qs.filter(is_staff=False)
+            qs = qs.filter(Q(is_staff=False) | Q(Q(is_staff=True) & Q(is_superuser=False)))
         return qs
